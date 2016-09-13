2463d8784e78da850949b40e3c617325fb2a40d9
Jade Vance <jadenicolevance@gmail.com>
2016-09-13 13:16:11 -0700

    initial commit

---
+++ asp-net-site/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="asp_net_site.MvcApplication" Language="C#" %>
---
+++ asp-net-site/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(asp_net_site.Startup))]
+namespace asp_net_site
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
